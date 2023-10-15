b0ad44d72f0f623a4db81634d6908a010d8fb5ac
Prashant Ranjan Singh <85955003+Prashant-ranjan-singh-123@users.noreply.github.com>
2023-10-15 10:07:07 -0700

    Update controller.py

--- controller.py
+++ controller.py
@@ -77,19 +77,20 @@ def senceBrightnessStart(window_name, brightness_window):
 
         if hands:
             lm = hands[0]['lmList']
+
             length, info, img = hd.findDistance(lm[8][0:2], lm[4][0:2], img)
-            blevel = np.interp(length, [15, 220], [0, 100])
-            val = np.interp(length, [0, 100], [400, 150])
+            blevel = np.interp(length, [30, 350], [0, 100])
+            val = np.interp(length, [30, 350], [400, 150])
 
             blevel = int(blevel)
 
             scb.set_brightness(blevel)
 
-            cv.rectangle(img, (20, 150), (85, 400), (0, 255, 255), 4)
-            cv.rectangle(img, (20, int(val)), (85, 400), (0, 0, 255), -1)
+            cv.rectangle(img, (50, 150), (85, 400), (0, 255, 255), 4)
+            cv.rectangle(img, (50, int(val)), (85, 400), (0, 0, 255), -1)
             cv.putText(img, str(blevel) + '%', (20, 430), cv.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 3)
 
-        cv.imshow(window_name, img)
+        cv2.imshow(window_name, img)
         key = cv2.waitKey(1)
         if key == 27:  # Pressing Esc key to close only this window
             cv2.destroyWindow(window_name)
